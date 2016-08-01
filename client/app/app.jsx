13f7624e2e11664a5daed547f8c69b1c049172b9
Matthias Brunet <matthias.brunet@viseo.com>
2016-08-01 20:35:28 +0200

    store provider added

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+const mock_store = {
+    subscribe: () => {},
+    dispatch: () => {},
+    getState: () => {}
+};
 
 export default class App extends React.Component {
     render() {
         return (
-            <section>
-                <h2 className="title">Here goes MC Mobile Site</h2>
-            </section>
+            <Provider store={mock_store}>
+                <section>
+                    <h2 className="title">Here goes MC Mobile Site</h2>
+                    {this.props.children}
+                </section>
+            </Provider>
         );
     }
 }
