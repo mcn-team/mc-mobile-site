800ef17f51d13ccf4080d77139e1cb4e752b7596
Matthias Brunet <matthias.brunet@viseo.com>
2016-08-01 20:20:28 +0200

    updating html index and general style
    gulpfile updated with css tasks
    overriding stylesheet added

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { render as Render } from 'react-dom';
 
 Render(
-    <h2>Here goes MC Mobile Site</h2>,
+    <h2 className="title">Here goes MC Mobile Site</h2>,
     document.getElementById('app')
 );
