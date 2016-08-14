12803bf1aff5dc8e43877529a6589e425959a1c9
Matthias Brunet <matthias.brunet@viseo.com>
2016-08-14 23:07:03 +0200

    details page components updated
    reset details action added to reducers and actions

--- client/app/components/details-page/details-reducers.js
+++ client/app/components/details-page/details-reducers.js
@@ -1,4 +1,4 @@
-import { FETCH_DETAILS_FAIL_TYPE, FETCH_DETAILS_START_TYPE, FETCH_DETAILS_SUCCESS_TYPE }
+import { FETCH_DETAILS_FAIL_TYPE, FETCH_DETAILS_START_TYPE, FETCH_DETAILS_SUCCESS_TYPE, FETCH_DETAILS_RESET_TYPE }
     from './details-actions';
 
 const defaultDetailsState = { response: [] };
@@ -11,6 +11,8 @@ const details = (state = defaultDetailsState, action) => {
             return Object.assign({}, state, { fetching: false, error: Object.assign({}, action.error) });
         case FETCH_DETAILS_SUCCESS_TYPE:
             return Object.assign({}, state, { fetching: false, response: [].concat(action.response) });
+        case FETCH_DETAILS_RESET_TYPE:
+            return Object.assign({}, defaultDetailsState);
         default:
             return state;
     }
