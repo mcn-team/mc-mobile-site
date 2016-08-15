46bfde8263a46f20292be6944235654dba93aef9
Matthias Brunet <matthias.brunet@viseo.com>
2016-08-15 23:28:03 +0200

    missing pages component update
    lodash added (thanks the gods)
    loop error on find missing fixed
    collections with books registered as not bought now displays a adequate message

--- client/app/components/details-page/missing-list.jsx
+++ client/app/components/details-page/missing-list.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 
 import LastMedia from './last-media';
 import MissingMedia from './missing-media';
@@ -17,31 +18,50 @@ export default class MissingList extends React.Component {
     }
 
     renderLastMedia() {
-        const media = this.props.list[this.props.list.length - 1];
+        const list = _.filter(this.props.list, { bought: true }).sort((a, b) => {
+            return a.volume - b.volume;
+        });
 
-        if (this.props.list && this.props.list.length > 0) {
+        let media = null;
+
+        if (list.length > 0) {
+            media = list[list.length - 1];
+        }
+
+        if (media) {
             return (
                 <LastMedia title={media.title} volume={media.volume} publisher={media.publisher}/>
             );
         } else {
-            return null;
+            return (
+                <div className="media-element column is-8-mobile is-offset-2-mobile">
+                    <p className="has-text-centered"><strong>This collection has no book already bought</strong></p>
+                    <p className="has-text-centered"><small>The first tome is in the list of books to buy</small></p>
+                </div>
+            );
         }
     }
 
     renderMissingMedia() {
         if (this.props.list && this.props.list.length > 0) {
+            const list = _.filter(this.props.list, { bought: true }).sort((a, b) => {
+                return a.volume - b.volume;
+            });
+            let lastVolume = this.props.list[this.props.list.length - 1].volume;
             let missing = [];
-            const lastVolume = this.props.list[this.props.list.length - 1].volume;
-            let i = 0;
             let volume = 1;
-            while (volume < lastVolume) {
-                if (volume === this.props.list[i].volume) {
-                    i++;
-                } else {
-                    missing.push(volume);
-                }
 
-                volume++;
+            if (list.length > 0) {
+                lastVolume = list[list.length - 1].volume;
+
+                while (volume < lastVolume) {
+                    const media = _.filter(this.props.list, { volume: volume });
+                    if (media.length === 0 || media[0].bought === false) {
+                        missing.push(volume);
+                    }
+
+                    volume++;
+                }
             }
 
             return (
