18ffcbf80204de6320e9a338894c78fa2ba602bd
Matthias Brunet <matthias.brunet@viseo.com>
2016-08-30 22:41:54 +0200

    loaders added on pages

--- client/app/components/collections-list-page/collections-list-page.jsx
+++ client/app/components/collections-list-page/collections-list-page.jsx
@@ -6,11 +6,14 @@ import HeaderComponent from '../commons/header';
 import { fetchCollectionAction, DISPLAY_COLLECTION } from './collections-list-actions';
 import { CollectionItem } from './collection-item';
 import { Authentication } from '../../utils/authentication-helper';
+import Loader from '../commons/loader';
 
 class CollectionsListPageComponent extends React.Component {
     constructor(props) {
         super(props);
+
         this.onClick = this.onClick.bind(this);
+        this.mainRendering = this.mainRendering.bind(this);
     }
 
     componentWillMount() {
@@ -31,27 +34,43 @@ class CollectionsListPageComponent extends React.Component {
         this.props.dispatch(DISPLAY_COLLECTION);
     }
 
+    mainRendering() {
+        let component = null;
+
+        console.log(this.props.collections);
+        if (this.props.collections.fetching) {
+            component = <Loader/>;
+        } else if (this.props.collections.response.length > 0) {
+            component = (
+                <section>
+                    <div className="has-text-centered spacer">
+                        <button type="button" className="button linear-grey is-small" onClick={this.onClick}>
+                            { this.props.collections.showCompleted ? "Show unfinished" : "Show all" }
+                        </button>
+                    </div>
+                    <div className="spacer column is-10-mobile is-offset-1-mobile has-text-centered">
+                        { this.props.collections.response.map((element, index) => {
+                            return (
+                                <CollectionItem
+                                    hidden={!this.props.collections.showCompleted && !element.isMissing && element.isCompleted}
+                                    completed={!element.isMissing && element.isCompleted}
+                                    key={index}
+                                    title={element._id}
+                                />
+                            );
+                        }) }
+                    </div>
+                </section>);
+        }
+
+        return component;
+    }
+
     render() {
         return (
             <section id="collections" className="columns is-marginless">
                 <HeaderComponent title="Media Collection" subtitle="Collections list" />
-                <div className="has-text-centered spacer">
-                    <button type="button" className="button linear-grey is-small" onClick={this.onClick}>
-                        { this.props.collections.showCompleted ? "Show unfinished" : "Show all" }
-                    </button>
-                </div>
-                <div className="spacer column is-10-mobile is-offset-1-mobile has-text-centered">
-                    { this.props.collections.response.map((element, index) => {
-                        return (
-                            <CollectionItem
-                                hidden={!this.props.collections.showCompleted && !element.isMissing && element.isCompleted}
-                                completed={!element.isMissing && element.isCompleted}
-                                key={index}
-                                title={element._id}
-                            />
-                        );
-                    }) }
-                </div>
+                { this.mainRendering() }
             </section>
         );
     }
